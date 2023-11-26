613860d61b95e3a740b2cb7a5a6c05645295e79e
RoyaAlmasimehr <almasimehrroya@gmail.com>
2023-11-26 12:39:00 +0330

    change source

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ function App() {
       </header>
       <footer>
         This is project was coded by Roya Almasimehr and is{" "}
-        <a href="https://github.com/RoyaAlmasimehr/weather-react">
+        <a href="https://github.com/RoyaAlmasimehr/shecode-react">
           open-sourced on GitHub{" "}
-        </a>and
-        <a href="https://sweet-cranachan-61f88c.netlify.app/">
+        </a>
+        and
+        <a href="https://eloquent-dragon-479dc5.netlify.app//">
           hosted on Netlify
         </a>
       </footer>
