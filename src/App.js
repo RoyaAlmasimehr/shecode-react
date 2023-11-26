1bdad288310cb63ea101ef658715680f031fd766
RoyaAlmasimehr <almasimehrroya@gmail.com>
2023-11-26 12:00:55 +0330

    Added weather js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather from "./weather.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Weather defaultCity="Tehran" />
       </header>
+      <footer>
+        This is project was coded by Roya Almasimehr and is{" "}
+        <a href="https://github.com/RoyaAlmasimehr/weather-react">
+          open-sourced on GitHub and{" "}
+          <a href="https://sweet-cranachan-61f88c.netlify.app/">
+            hosted on Netlify
+          </a>
+        </a>
+      </footer>
     </div>
   );
 }
